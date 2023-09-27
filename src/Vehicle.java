f61f82a96498d804c821b0266a4ab8775e372d72
Ekaterina <klarina2409@gmail.com>
2023-09-27 21:50:34 +0300

    HomeWork

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,4 +1,20 @@
 public class Vehicle {
-    public String modelName;
-    public int wheelsCount;
+    private String modelName;
+    private int wheelsCount;
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
 }
